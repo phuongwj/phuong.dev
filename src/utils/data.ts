6b9f80243cabcd758e8db6f8752f0f530f907151
phuongwj <phamjulia9@gmail.com>
2026-03-20 23:36:31 -0300

    some adjustments

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,30 +1,52 @@
+export const techColors: Record<string, string> = {
+    "Java": "#F89820",
+    "JavaScript": "#F7DF1E",
+    "TypeScript": "#3178C6",
+    "React": "#61DAFB",
+    "Vue.js": "#42B883",
+    "Go": "#00ADD8",
+    "PostgreSQL": "#4169E1",
+    "Sass": "#CC6699",
+    "Framer Motion": "#E040FB",
+    "i18n": "#8BC34A",
+    "Play Framework": "#93D500",
+    "KnockoutJS": "#E42E16",
+    "jQuery": "#0769AD",
+    "Spring": "#6DB33F",
+    "Docker": "#2496ED",
+};
+
 export const experience = [
     {
         company: "Dalhousie University",
         position: "Web Developer Intern",
         duration: "6/2024 - 8/2024",
         image: "/logos/logo_dal.png",
-        description: "One line description here"
+        description: "One line description here",
+        stack: ["JavaScript", "React", "Sass", "Framer Motion", "i18n"]
     },
     {
         company: "Dalhousie University",
         position: "Undergraduate Teaching Assistant",
         duration: "9/2024 - 5/2025",
         image: "/logos/logo_dal.png",
-        description: "One line description here"
+        description: "One line description here",
+        stack: ["Java", "JavaScript"]
     },
     {
         company: "SpryPoint",
         position: "Software Developer Intern",
         duration: "5/2025 - 8/2025",
         image: "/logos/logo_sprypoint.png",
-        description: "One line description here"
+        description: "One line description here",
+        stack: ["Java", "JavaScript", "Play Framework", "KnockoutJS", "jQuery"]
     },
     {
         company: "PRAXES Medical Group",
         position: "Full-Stack Developer Intern",
         duration: "1/2026 - Present",
         image: "/logos/logo_praxes.jpg",
-        description: "One line description here"
+        description: "One line description here",
+        stack: ["Go", "TypeScript", "Vue.js", "PostgreSQL"]
     }
 ]
